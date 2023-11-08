2f29ff2929bca937ac5d0f5ed272f4805cd021fb
rurigrass <ruripg@gmail.com>
2023-11-08 23:36:53 +0100

    styling in front page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ import { ForwardedRef, Ref, forwardRef } from "react";
 
 const Header = forwardRef(function index(props, ref: any) {
   return (
-    <div className=" fixed flex w-full justify-end p-[10px] box-border mix-blend-difference z-10">
+    <div className="fixed flex w-full justify-end p-[10px] box-border mix-blend-difference z-10">
       <Magnetic>
-        <div className=" relative cursor-pointer flex gap-[8px] flex-col p-[30px] pointer-events-none before:block before:w-[30px] before:h-[2px] before:mix-blend-difference before:bg-white after:block after:w-[30px] after:h-[2px] after:mix-blend-difference after:bg-white">
+        <div className="relative cursor-pointer flex gap-[8px] flex-col p-[30px] pointer-events-none before:block before:w-[30px] before:h-[2px] before:mix-blend-difference before:bg-white after:block after:w-[30px] after:h-[2px] after:mix-blend-difference after:bg-white">
           <div
             ref={ref}
             className="absolute left-0 top-0 w-full h-full pointer-events-auto hover:scale-150"
