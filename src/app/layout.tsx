3556c4866f2f9bf3afab86bb1b4714fba8e85cea
rurigrass <ruripg@gmail.com>
2024-03-08 16:58:07 +0100

    removed the touch action

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout(props: RootLayoutProps) {
   return (
     <html
       lang="en"
-      className={`touch-pan-y ${fonts.adventPro.variable} ${fonts.belleza.variable} ${fonts.capriola.variable} ${fonts.caudex.variable} ${fonts.cinzelDecorative.variable} ${fonts.dmSans.variable} ${fonts.federo.variable} ${fonts.kronaOne.variable} ${fonts.roboto.variable} ${fonts.spaceGrotesk.variable} ${fonts.syne.variable} ${fonts.italiana.variable}`}
+      className={`${fonts.adventPro.variable} ${fonts.belleza.variable} ${fonts.capriola.variable} ${fonts.caudex.variable} ${fonts.cinzelDecorative.variable} ${fonts.dmSans.variable} ${fonts.federo.variable} ${fonts.kronaOne.variable} ${fonts.roboto.variable} ${fonts.spaceGrotesk.variable} ${fonts.syne.variable} ${fonts.italiana.variable}`}
     >
       <body>{props.children}</body>
     </html>
